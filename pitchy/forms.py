8f83f1d9726aad520852bff55b52e8820d4b64c7
Briana Eng <brianaeng@gmail.com>
2017-01-06 10:05:52 -0800

    Made a couple more fields in the sign up page required.

--- pitchy/forms.py
+++ pitchy/forms.py
@@ -20,7 +20,9 @@ class FocusForm(forms.ModelForm):
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
