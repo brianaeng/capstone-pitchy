ed873cd6b01a3e34955318990fa737463ae875ec
Briana Eng <brianaeng@gmail.com>
2017-01-09 11:01:40 -0800

    Commenting out templates

--- pitchy/urls.py
+++ pitchy/urls.py
@@ -3,14 +3,14 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
-    url(r'^$', views.HomepageView.as_view(), name='homepage'),
-    url(r'^profile/edit/$', views.UpdateProfileView.as_view(), name='update_profile'),
-    url(r'^hub/$', views.HubView.as_view(), name='hub'),
-    url(r'^user/(?P<pk>\d+)/$', views.ProfileView.as_view(), name='profile'),
-    url(r'^messages/(?P<pk>\d+)/$', views.ConversationView.as_view(), name='messages'),
+    # url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    # url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    # url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
+    # url(r'^$', views.HomepageView.as_view(), name='homepage'),
+    # url(r'^profile/edit/$', views.UpdateProfileView.as_view(), name='update_profile'),
+    # url(r'^hub/$', views.HubView.as_view(), name='hub'),
+    # url(r'^user/(?P<pk>\d+)/$', views.ProfileView.as_view(), name='profile'),
+    # url(r'^messages/(?P<pk>\d+)/$', views.ConversationView.as_view(), name='messages'),
     # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     # url(r'^post/new/$', views.post_new, name='post_new'),
     # url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
