9d4aa86e3d2aaa6efb995d3a6fd295afa9bd5e54
Briana Eng <brianaeng@gmail.com>
2017-01-14 17:28:16 -0800

    cosmetic changes

--- pitchy/models.py
+++ pitchy/models.py
@@ -70,7 +70,7 @@ class DirectMessage(models.Model):
 
     @property
     def formatted_timestamp(self):
-        return self.sent_at.strftime('%b %-d %-I:%M %p')
+        return self.sent_at.strftime('%m/%-d %-I:%M %p')
 
     def as_dict(self):
         return {'sender': self.sender, 'body': self.body, 'sent_at': self.formatted_timestamp}
