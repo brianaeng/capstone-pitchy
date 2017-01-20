d5704ad22a41697cf368c88901242d64fc515d18
Briana Eng <brianaeng@gmail.com>
2017-01-20 15:11:29 -0800

    static update

--- capstone/settings.py
+++ capstone/settings.py
@@ -146,8 +146,8 @@ LOGIN_REDIRECT_URL = 'homepage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
