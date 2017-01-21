8041fa92ac153ed544c29d20d18300817997e87b
Briana Eng <brianaeng@gmail.com>
2017-01-21 13:59:33 -0800

    local dev update

--- capstone/settings.py
+++ capstone/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'getpitchy.herokuapp.com', 'www.getpitchy.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'getpitchy.herokuapp.com', 'www.getpitchy.com']
 
 # Application definition
 
