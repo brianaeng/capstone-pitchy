6001e17e11f1129cf1aa772dd3a330ec2091aa85
Briana Eng <brianaeng@gmail.com>
2017-01-23 08:45:26 -0800

    search results go away if backspaced completely

--- static/search.js
+++ static/search.js
@@ -1,16 +1,22 @@
 $(function (){
   $('#search').keyup(function(e) {
-    if ((e.which <= 90 && e.which >= 48 || e.which == 8) && $('#search').val() !== '') {
-      $.ajax({
-        type: "POST",
-        url: "/search/",
-        data: {
-          'search_text': $('#search').val(),
-          'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val()
-        },
-        success: searchSuccess,
-        dataType: 'html'
-      });
+    if (e.which <= 90 && e.which >= 48 || e.which == 8) {
+      if ($('#search').val() === '') {
+        $('#search-results').html("");
+        console.log("clearing");
+      }
+      else {
+        $.ajax({
+          type: "POST",
+          url: "/search/",
+          data: {
+            'search_text': $('#search').val(),
+            'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val()
+          },
+          success: searchSuccess,
+          dataType: 'html'
+        });
+      }
     }
   });
 });
